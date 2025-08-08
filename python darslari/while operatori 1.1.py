49095ddeec4756c039d273c87e4ce65c03fc5158
Mardonbek Usmonov <150879915+MardonbekUsmonov@users.noreply.github.com>
2025-08-08 19:05:24 +0500

    yangi dars qoshildi

---
+++ python darslari/while operatori 1.1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  8 18:34:38 2025
+
+@author: user
+"""
+# son = 1
+# while son<=10: #toki son 10 dan kichik yoki teng bo'lguncha bajar
+#     print(son, end=" ")
+#     son+=1 # son = son + 1 
+# print("\ndastur tugadi")
+
+
+# print("\t\t\t...Istalgan sonni kvadratini qaytaruvchi dastur...")
+# savol = "Istalgan sonni kiriting : \n (Dasturni to'xtatish uchun 'exit' deb yozing )"
+# qiymat = ' '
+# while qiymat != 'exit':
+#     qiymat = input(savol)
+#     if qiymat != 'exit':
+#         print(f"{float(qiymat)} ning kvadrati {float(qiymat)**2} ga teng")
+# print("dastur tugadi !")
+
